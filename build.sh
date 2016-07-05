e44a33d8facfed9222d1855a16171484fed24220
Eric J. Ma <ericmajinglong@gmail.com>
2016-07-05 09:44:16 -0400

    Modified script to use default.latex template and open the files right after compiling.

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-pandoc 01-research-statement.md -o "01-research-statement.pdf" -V "geometry:left=0.5in, right=0.5in, bottom=1in, top=0.5in" --latex-engine="/Library/TeX/texbin/xelatex" --filter="/Users/ericmjl/anaconda/bin/pandoc-fignos" --filter="/usr/local/bin/pandoc-citeproc" --bibliography="/Users/ericmjl/Documents/github/writing/references-master/papers-library.bib"
+pandoc 01-research-statement.md -o "01-research-statement.pdf" -V "geometry:left=0.5in, right=0.5in, bottom=1in, top=0.5in" --latex-engine="/Library/TeX/texbin/xelatex" --filter="/Users/ericmjl/anaconda/bin/pandoc-fignos" --filter="/usr/local/bin/pandoc-citeproc" --bibliography="/Users/ericmjl/Documents/github/writing/references-master/papers-library.bib" --template="/Users/ericmjl/Documents/github/writing/pandoc-templates/default.latex"
 
 pandoc 02-cv.md -o "02-cv.pdf" -V "geometry:left=0.5in, right=0.5in, bottom=1in, top=0.5in" --latex-engine="/Library/TeX/texbin/xelatex"  --bibliography="/Users/ericmjl/Documents/github/writing/references-master/papers-library.bib"
+
+open "01-research-statement.pdf"
+open "02-cv.pdf"
